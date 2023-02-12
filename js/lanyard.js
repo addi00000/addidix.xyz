79816ecc2f648bf157cdef20013f183bd6725783
addi00000 <81730200+addi00000@users.noreply.github.com>
2023-02-12 17:02:25 -0500

    Update lanyard.js

--- js/lanyard.js
+++ js/lanyard.js
@@ -1,5 +1,5 @@
 lanyard({
-    userId: "1037686160622878771",
+    userId: "1072675217270378666",
 }).then((response) => {
     document.getElementsByClassName("lanyard-profile-picture")[0].src = `https://cdn.discordapp.com/avatars/${response.discord_user.id}/${response.discord_user.avatar}.png`;
     document.getElementsByClassName("lanyard-username")[0].innerHTML = `${response.discord_user.username}#${response.discord_user.discriminator}`;
